53cf87ea3c2f1fd735a41f5131fd95551d16df55
tegamex <kpruiz11@gmail.com>
2021-11-30 17:05:33 -0500

    Unit Testing

---
+++ ProgramTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Nama.Data;
+using System;
+
+namespace Nama
+{
+    public class ProgramTest
+    {
+        [Fact]
+        public void testing1()
+        {
+            Assert.False(false, "Account not debited correctly");
+        }
+    }
+}
